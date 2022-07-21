4a7a3f0b9c358651e0b0915431cdeecef850359c
Kate Lee <isoeun809@gmail.com>
2022-07-21 14:21:49 -0500

    Initial Commit

---
+++ Source Control Test/Source_Control_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_Control_TestApp.swift
+//  Source Control Test
+//
+//  Created by 이소은 on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Source_Control_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
